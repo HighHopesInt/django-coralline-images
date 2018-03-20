059a48b8b4fc189e99c1440f443c2c6f9b6135eb
ukhryab <mihail.sibirev@mail.ru>
2018-03-20 19:00:31 +0300

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='coralline-userimages',
+    version='0.1',
+    packages=['UserImages', 'UserImages.migrations'],
+    url='https://github.com/HighHopesInt/coralline-userimages',
+    license='',
+    author='Mikhail Sibiryov',
+    author_email='m.sibirev@wis.software',
+    description='A package, containing user\'s docker images django model. '
+)
