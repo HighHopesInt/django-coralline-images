b4f5e12963d54ea76c9db7b4be14eeaaedabd18c
ukhryab <mihail.sibirev@mail.ru>
2018-03-22 19:30:48 +0300

    Change version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-coralline-images',
-    version='0.1',
+    version='0.2',
     packages=['django_coralline_images', 'django_coralline_images.migrations'],
     url='https://github.com/HighHopesInt/django-coralline-images',
     license='',
