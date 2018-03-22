e84b70943750d77d57e92d45512dbfc0ead28d70
ukhryab <mihail.sibirev@mail.ru>
2018-03-22 19:09:02 +0300

    Add initial settings, setup settings.

--- django_coralline_images/__init__.py
+++ django_coralline_images/__init__.py
@@ -1,2 +1,10 @@
 """This is a package for using by other parts of Coralline project.
 Detailed description pending."""
+
+import os
+import django
+from django.conf import settings
+
+if not settings.configured:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_coralline_images.settings")
+    django.setup()
---
+++ django_coralline_images/settings.py
@@ -0,0 +1,29 @@
+import os
+
+DEBUG = True
+
+DEFAULT_INDEX_TABLESPACE = ''
+
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.mysql',
+       'NAME': os.getenv('NAME'),
+       'USER': os.getenv('USER'),
+       'PASSWORD': os.getenv('PASSWORD'),
+       'HOST': os.getenv('HOST'),
+       'PORT': os.getenv('PORTDB'),
+   }
+}
+
+SECRET_KEY = 'zzz'
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.contenttypes',
+
+    'django_coralline_images',
+]
